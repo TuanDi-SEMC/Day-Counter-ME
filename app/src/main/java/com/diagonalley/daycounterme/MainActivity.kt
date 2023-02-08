52cef1d2f39cce1a7f1304e59d2205127d0fc01a
TuanTM <nazik.vg@gmail.com>
2023-02-08 16:52:43 +0700

    Untrack files in .gitignore

--- app/src/main/java/com/diagonalley/daycounterme/MainActivity.kt
+++ app/src/main/java/com/diagonalley/daycounterme/MainActivity.kt
@@ -2,8 +2,15 @@ package com.diagonalley.daycounterme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.diagonalley.daycounterme.global.AppConfig
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var appConfig: AppConfig
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
